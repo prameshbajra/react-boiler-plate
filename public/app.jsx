3a67265bb67c7bd1fa1ab94819ae50c6eab20aa2
Suzal3579 <pe.messh@gmail.com>
2017-11-20 16:40:41 +0530

    create basic components !! XD happy happy

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,15 @@
+const Helloer = React.createClass({
+    render: () => {
+        return (
+            <div>
+                <h1>Suzal hero nai xa</h1>
+                <p>Bajracharya haru sapai hero nai hunnxa !</p>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Pramesh Bajracharya</h1>,
+    <Helloer />,
     document.getElementById("app")
 );
