c19486500e85b8700b3eacd9bc11f562c2535bbf
Suzal3579 <pe.messh@gmail.com>
2017-11-20 18:42:01 +0530

    Introduction to states ...

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,34 @@
 const Invite = React.createClass({
+    // Props bata data pass vayena vanney you default value hunnxa ...
     getDefaultProps: function () {
         return {
             name: "Pramesh"
         }
     },
+    // State ko lai initial you tala ko method hunnxa ...
+    getInitialState: function () {
+        return {
+            name: this.props.name
+        }
+    },
+    handleForm: function (e) {
+        e.preventDefault();
+        const name = this.refs.inputName.value;
+        this.refs.inputName.value = "";
+        // Updates the state and rerenders ...
+        this.setState({
+            name: name
+        });
+    },
     render: function () {
         return (
             <div>
-                <h1>{this.props.name}</h1>
+                <h1>{this.state.name}</h1>
                 <h1>{this.props.message}</h1>
+                <form onSubmit={this.handleForm}>
+                    <input type="text" ref="inputName" />
+                    <button>Set name</button>
+                </form>
             </div>
         );
     }
